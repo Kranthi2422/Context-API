a332023044e47ba647f0240695840039d7326089
kcube2 <karanamkranthikumar22@gmail.com>
2022-01-23 23:04:59 +0530

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
 
-function App() {
+import Provider from "./provider";
+
+import Context from "./context";
+
+const Agents = () => {
+  return <AgentOne />;
+};
+
+const AgentOne = () => {
+  return <AgentTwo />;
+};
+
+const AgentTwo = () => {
+  return <AgentBond />;
+};
+
+const AgentBond = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Context.Consumer>
+      {(context) => (
+        <Fragment>
+          <h3>Agent Info : {context.data.agent}</h3>
+          <p>Mission Name : {context.data.mname}</p>
+          <p>Status: {context.data.accept}</p>
+          <button onClick={context.isMissionAccepted}> Click To Accept </button>
+        </Fragment>
+      )}
+    </Context.Consumer>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Context API</h1>
+      <Provider>
+        <Agents />
+      </Provider>
     </div>
   );
-}
+};
 
 export default App;
